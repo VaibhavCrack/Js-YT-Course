2e5940aa9051e6f508cf075460c1574bad6aaeed
VaibhavCrack <rekhamalkoti995@gmail.com>
2024-07-24 06:39:39 +0000

    Little Practice

--- 01_Basics/ops.js
+++ 01_Basics/ops.js
@@ -1,3 +1,8 @@
-let x = 3 
-let negvalue = -x 
-console.log(negvalue);
+// let x = 3 
+// let negvalue = -x 
+// console.log(negvalue);
+
+let str1 = "Hi"
+let str2 = " Vaibhav"
+let str3 = str1 + str2 
+console.log(str3)
--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,5 @@
-console.log("Vaibhav")
+const age = 13 
+let naam = "VaibhavMalkoti"
+let Height = 5.5 
+console.table([age , naam  , Height])
+console.log(typeof(Height))
